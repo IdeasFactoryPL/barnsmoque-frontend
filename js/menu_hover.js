69b44685d392d0dff67bb4e9aedb0a7b9da7e744
Rafał Petryka <rafalpetryka@gmail.com>
2015-09-12 18:41:42 +0200

    Added images, fancybox(wip), menu(wip)

--- js/menu_hover.js
+++ js/menu_hover.js
@@ -6,6 +6,34 @@ $('#menu .wrapper div figure').hover(function(){
 	$(this).children("figcaption").removeClass("figcaption_hover");
 });
 
-$('.menu_button').click(function(){
-	debugger;
+$('.menu_button').click(function() {
+    $('nav').animate({
+    	left: "0px"
+    }, 200);
+    $('body').animate({
+    	left: "285px"
+    }, 200);
+    $.fn.animatescroll.defaults = {        
+        easing:"swing",
+        scrollSpeed:800,
+        padding:0,
+        element:"html,body"
+    };
+});
+/* Then push them back */
+$('.icon-close').click(function() {
+    $('nav').animate({
+    	left: "-285px"
+    }, 200);
+    $('body').animate({
+    	left: "0px"
+    }, 200);
+});
+$('li').click(function() {
+    $('nav').animate({
+    	left: "-285px"
+    }, 200);
+    $('body').animate({
+    	left: "0px"
+    }, 200);
 });
--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(window).scroll(function () {
-    if ($(window).scrollTop() > $(window).height() - $('nav').height()) {
+    if ($(window).scrollTop() > $(window).height() - $('nav').height() && $(window).width() > 900) {
       $('nav').addClass('navbar-fixed');
       $('#o_restauracji').addClass('add_margin_to_section');
     }
