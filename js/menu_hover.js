bda2826128f86f7cd8b3f74efe5e0b349bac3907
Rafał Petryka <rafalpetryka@gmail.com>
2015-09-13 10:57:04 +0200

    Responsive menu almost finished

--- js/menu_hover.js
+++ js/menu_hover.js
@@ -9,10 +9,18 @@ $('#menu .wrapper div figure').hover(function(){
 $('.menu_button').click(function() {
     $('nav').animate({
     	left: "0px"
-    }, 200);
+    }, 500);
+
+    $('nav').css({
+   		display: "block"
+   		// position: "fixed"
+    });
     $('body').animate({
     	left: "285px"
-    }, 200);
+    }, 500);
+    $('.menu_button').animate({
+    	left: '-60px'
+    });
     $.fn.animatescroll.defaults = {        
         easing:"swing",
         scrollSpeed:800,
@@ -21,19 +29,68 @@ $('.menu_button').click(function() {
     };
 });
 /* Then push them back */
-$('.icon-close').click(function() {
+$('.close_nav').click(function() {
     $('nav').animate({
     	left: "-285px"
-    }, 200);
+    }, 500);
+    $('nav').css({
+    	display: "none"
+    });
     $('body').animate({
     	left: "0px"
-    }, 200);
+    }, 500);
+    $('.navbar-fixed').css({
+    	left: "0px"
+    });
+    $('.menu_button').animate({
+    	left: '15px'
+    });
 });
 $('li').click(function() {
-    $('nav').animate({
-    	left: "-285px"
-    }, 200);
-    $('body').animate({
-    	left: "0px"
-    }, 200);
+	if($(window).width() <= 900){
+	    $('nav').animate({
+	    	left: "-285px"
+	    }, 500);
+	    $('body').animate({
+	    	left: "0px"
+	    }, 500);
+	    $('.menu_button').animate({
+	    	left: '15px'
+	    });
+	}
+});
+
+$(window).resize(function() {
+	if($(window).width() > 900){
+		$('nav').css({
+	   		display: "block",
+	   		position: "fixed",
+	   		left: '0px'
+	    });
+	    $('.menu_button').animate({
+			left: '-60px'
+	    }, 500);
+	    $('nav').addClass('navbar-fixed');
+	    $.fn.animatescroll.defaults = {
+	        easing:"swing",
+	        scrollSpeed:800,
+	        padding:61,
+	        element:"html,body"
+	    };
+	}else{
+		$('nav').css({
+	   		display: "none",
+	   		position: "fixed"
+	    });
+	    $('.menu_button').animate({
+	    	left: '15px'
+	    }, 500);
+	    $('nav').removeClass('navbar-fixed');
+        $.fn.animatescroll.defaults = {
+	        easing:"swing",
+	        scrollSpeed:800,
+	        padding:0,
+	        element:"html,body"
+	    };
+	}
 });
--- js/nav.js
+++ js/nav.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
   $(window).scroll(function () {
     if ($(window).scrollTop() > $(window).height() - $('nav').height() && $(window).width() > 900) {
       $('nav').addClass('navbar-fixed');
-      $('#o_restauracji').addClass('add_margin_to_section');
     }
     if ($(window).scrollTop() < $(window).height() - $('nav').height() + 1) {
       $('nav').removeClass('navbar-fixed');
-      $('#o_restauracji').removeClass('add_margin_to_section');
     }
   });
 });
